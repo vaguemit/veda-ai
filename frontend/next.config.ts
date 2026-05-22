e4f7debab873d3421dfca0f390ad4410a947028b
vaguemit <mitparikh1046@gmail.com>
2026-05-22 17:22:23 +0530

    Update turbopack root to use process.cwd

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from "next";
-import { fileURLToPath } from "url";
-import path from "path";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const nextConfig: NextConfig = {
   turbopack: {
-    root: path.resolve(__dirname),
+    root: process.cwd(),
   },
 };
 

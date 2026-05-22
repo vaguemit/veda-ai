52801f5f357b4fe54d6236d830c04b4c3bb8f74f
vaguemit <mitparikh1046@gmail.com>
2026-05-22 17:21:57 +0530

    Configure turbopack root to frontend folder in next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: path.resolve(__dirname),
+  },
 };
 
 export default nextConfig;
